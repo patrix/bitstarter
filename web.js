3a32a7ae1a34a7a5a89afce3e86a2f9feeb3253f
Ubuntu <ubuntu@ip-10-194-78-154.ec2.internal>
2013-07-11 01:04:31 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var buf = new Buffer(html);
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-  response send(html);
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
