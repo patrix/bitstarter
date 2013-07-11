7011e79d2d926fc395917c076152f1b47c79ef1d
Ubuntu <ubuntu@ip-10-194-78-154.ec2.internal>
2013-07-11 01:43:09 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,14 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 //var html = fs.readFileSync('index.html');
-//var buf = new Buffer(html);	
+var buf = new Buffer(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-var content;
-var fs = require('fs');
-var buff = new Buffer(1024);
-response.send(buffer.toString('utf-8', fs.readFileSync('index.html')))});
+//var content;
+//var fs = require('fs');
+//var buff = new Buffer(fs.readFileSync('index.html1024);
+response.send(buffer.toString());});
+//)});
 //, function (err, data) {
 //content = data;
 //});
